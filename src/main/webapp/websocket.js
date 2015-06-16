c182b535f99dfa853e3f671a18f08c937262cc4a
tatianomnom <orangium@gmail.com>
2015-06-16 17:31:21 +0300

    Message log is sorted like... a normal message log

--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -5,20 +5,16 @@ var WS = function () {
     return  {
         disconnect: function (name) {
             if (name in sockets) sockets[name].close();
-            document.getElementById('log').removeChild(document.getElementById(name))
         },
         send: function (name, msg) {
             if (name in sockets) sockets[name].send(msg)
         },
         connectChannel: function(name) {
             var socket = new WebSocket("ws://localhost:8080/notifoo/channel/" + name);
-            var channelDiv = document.createElement("div");
-            channelDiv.id = name;
-            document.getElementById('log').appendChild(channelDiv);
             socket.onmessage = function(event) {
                 var entry = document.createElement("p");
                 entry.innerHTML = event.data;
-                document.getElementById(name).appendChild(entry);
+                document.getElementById('log').appendChild(entry);
             };
             sockets[name] = socket;
         }
