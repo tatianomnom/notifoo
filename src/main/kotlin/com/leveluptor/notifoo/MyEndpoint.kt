54bbad234ad10ef174d2e44b78c64feac1e77e15
tatianomnom <orangium@gmail.com>
2015-06-16 17:23:45 +0300

    user correctly sees only messages from his channels

--- src/main/kotlin/com/leveluptor/notifoo/MyEndpoint.kt
+++ src/main/kotlin/com/leveluptor/notifoo/MyEndpoint.kt
@@ -1,31 +1,43 @@
 package com.leveluptor.notifoo;
 
+import java.util.HashMap
+import java.util.HashSet
 import javax.websocket.OnClose
 import javax.websocket.OnMessage
 import javax.websocket.OnOpen
 import javax.websocket.Session
 import javax.websocket.server.PathParam
 import javax.websocket.server.ServerEndpoint
+import kotlin.platform.platformStatic
+
+private val subscribers = HashMap<String, Set<Session>>()
 
 @ServerEndpoint(value = "/channel/{channel}")
 public class MyEndpoint {
 
     @OnOpen
     public fun onOpen(@PathParam("channel") channel: String, session: Session) {
-        println(session.getId() + " connected")
-        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()} joined") }
+        (subscribers.getOrPut(channel, { hashSetOf() }) as HashSet).add(session)
+
+        val text = "[$channel] User ${session.getId()} joined"
+        println(text)
+
+        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText(text) }
     }
 
     @OnMessage
     public fun onMessage(message: String, @PathParam("channel") channel: String, session: Session) {
-        println(session.getId() + " sent " + message)
-        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()}: $message") }
+        val text = "[$channel] User ${session.getId()}: $message"
+        println(text)
+        subscribers.getOrDefault(channel, emptySet()).forEach { s -> s.getBasicRemote().sendText(text) }
     }
 
     @OnClose
     public fun onClose(@PathParam("channel") channel: String, session: Session) {
-        println(session.getId() + " disconnected")
-        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()} left") }
+        val text = "[$channel] User ${session.getId()} left"
+        println(text)
+        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText(text) }
+        (subscribers.getOrPut(channel, { hashSetOf() }) as HashSet).remove(session)
     }
 
 }
