f205977230b511f5e1e11e9cab66d62924e4695c
tatianomnom <orangium@gmail.com>
2015-06-16 16:12:24 +0300

    multiple channels kind of working

--- src/main/kotlin/com/leveluptor/notifoo/MyEndpoint.kt
+++ src/main/kotlin/com/leveluptor/notifoo/MyEndpoint.kt
@@ -4,25 +4,28 @@ import javax.websocket.OnClose
 import javax.websocket.OnMessage
 import javax.websocket.OnOpen
 import javax.websocket.Session
+import javax.websocket.server.PathParam
 import javax.websocket.server.ServerEndpoint
 
-@ServerEndpoint(value = "/demo")
+@ServerEndpoint(value = "/channel/{channel}")
 public class MyEndpoint {
 
     @OnOpen
-    public fun onOpen(session: Session) {
+    public fun onOpen(@PathParam("channel") channel: String, session: Session) {
         println(session.getId() + " connected")
+        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()} joined") }
     }
 
     @OnMessage
-    public fun onMessage(message: String, session: Session) {
+    public fun onMessage(message: String, @PathParam("channel") channel: String, session: Session) {
         println(session.getId() + " sent " + message)
-        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[User ${session.getId()}]: $message") }
+        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()}: $message") }
     }
 
     @OnClose
-    public fun onClose(session: Session) {
+    public fun onClose(@PathParam("channel") channel: String, session: Session) {
         println(session.getId() + " disconnected")
+        session.getOpenSessions().forEach { s -> s.getBasicRemote().sendText("[$channel] User ${session.getId()} left") }
     }
 
 }
--- src/main/webapp/websocket.js
+++ src/main/webapp/websocket.js
@@ -1,21 +1,25 @@
 var WS = function () {
 
-    var socket;
+    var sockets = {};
 
     return  {
-        connect: function () {
-            socket = new WebSocket("ws://localhost:8080/notifoo/demo");
+        disconnect: function (name) {
+            if (name in sockets) sockets[name].close();
+        },
+        send: function (name, msg) {
+            if (name in sockets) sockets[name].send(msg)
+        },
+        connectChannel: function(name) {
+            var socket = new WebSocket("ws://localhost:8080/notifoo/channel/" + name);
+            var channelDiv = document.createElement("div");
+            channelDiv.id = name;
+            document.getElementById('log').appendChild(channelDiv);
             socket.onmessage = function(event) {
                 var entry = document.createElement("p");
                 entry.innerHTML = event.data;
-                document.getElementById('log').appendChild(entry);
-            }
-        },
-        disconnect: function () {
-            socket.close()
-        },
-        send: function (msg) {
-            socket.send(msg)
+                document.getElementById(name).appendChild(entry);
+            };
+            sockets[name] = socket;
         }
     };
 }();
